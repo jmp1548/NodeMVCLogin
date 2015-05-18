91e14811c9c08fbb6531e40506f6889e4d1e6309
jmp1548 <jmp1548@rit.edu>
2015-05-18 10:48:18 -0400

    added warted and end adventure

--- client/map.js
+++ client/map.js
@@ -21,6 +21,7 @@ function showCoords(position) {
     var lat = position.coords.latitude;
     var long = position.coords.longitude;
     var path = poly.getPath();
+
     map.setZoom(30);
 
     locations.push({
@@ -28,14 +29,17 @@ function showCoords(position) {
     });
 
     var startInfowindow = new google.maps.InfoWindow({
+        content: "<h3>Start Position</h3><br/><strong>Latitute: </strong><small>" + lat + "</small><br/><strong>Longitude: </strong><small>" + long + "</small>"
     });
 
     var marker = new google.maps.Marker({
         position: locations[0].latlng,
+        animation: google.maps.Animation.DROP,
         map: map
     });
 
     google.maps.event.addListener(marker, 'click', function () {
+        startInfowindow.open(map, marker);
     });
 
     for (var i = 0; i < locations.length; i++) {
@@ -47,10 +51,26 @@ function showCoords(position) {
     }
 }
 
+function geoError(error) {
+    //erorr 
+    alert('ERROR(' + error.code + '): ' + error.message);
+
+    //alert("");
 }
 
 function init() {
+    //setInterval(function () {
+    //navigator.geolocation.getCurrentPosition(showCoords, geoError, geoOptions);
+    //}, 1000);
 
+    if (!navigator.geolocation) {
+        output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+        return;
+    }
+
+    watchId = GeolocationThrottle.watchPosition(showCoords, geoError, geoOptions), {
+        throttleTime: 5000
+    };
 
     var mapOptions = {
         center: {
@@ -58,6 +78,7 @@ function init() {
             lng: -98.579404
         },
         zoom: 3,
+        disableDefaultUI: true,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     map = new google.maps.Map(document.getElementById('map-div'), mapOptions);
@@ -71,5 +92,13 @@ function init() {
     poly = new google.maps.Polyline(polyOptions);
     poly.setMap(map);
 }
+
+function clearWatch(watchId) {
+    navigator.geolocation.clearWatch(watchId);
+}
+
+$('#endAdventure').on('click', function () {
+    clearWatch(watchId);
+});
 
 window.onload = init;
